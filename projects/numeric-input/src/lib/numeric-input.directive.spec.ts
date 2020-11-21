c0c4271ebaf904bd42f0c981ff6e1339143f3cd8
Daniel Liverant <daniel.liverant@takeaway.com>
2020-11-21 13:32:48 +0200

    feat(numeric-input): add tests

---
+++ projects/numeric-input/src/lib/numeric-input.directive.spec.ts
@@ -0,0 +1,88 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component } from '@angular/core';
+import { NumericInputDirective } from './numeric-input.directive';
+
+describe('dlNumericInput', () => {
+  let testFixture: ComponentFixture<TestComponent>;
+  const eventAlphabetic = new KeyboardEvent('keydown', { key: 'a', cancelable: true });
+  const eventNumeric = new KeyboardEvent('keydown', { key: '3', cancelable: true });
+
+  afterEach(() => {
+    testFixture = null;
+  });
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [TestComponent, NumericInputDirective]
+    });
+  });
+
+  it('should allow key - regular input', () => {
+    const input = dispatchInputEvent(testFixture, eventAlphabetic, false);
+    expect(input.value).toEqual(eventAlphabetic.key);
+    expect(eventAlphabetic.defaultPrevented).toBeFalsy();
+  });
+
+  it('should allow key - input with directive', () => {
+    const input = dispatchInputEvent(testFixture, eventNumeric, true);
+    expect(input.value).toEqual(eventNumeric.key);
+    expect(eventNumeric.defaultPrevented).toBeFalsy();
+  });
+
+  it('should prevent key - input with directive', () => {
+    dispatchInputEvent(testFixture, eventAlphabetic, true);
+    expect(eventAlphabetic.defaultPrevented).toBeTruthy();
+  });
+
+  it('should parse value', () => {
+    const input = getInput(testFixture, true);
+    input.value = '12,41';
+    input.dispatchEvent(new Event('change'));
+    expect(input.value).toEqual('12.41');
+  });
+
+  it('should parse a negative value', () => {
+    const input = getInput(testFixture, true);
+    input.value = '-12,41';
+    input.dispatchEvent(new Event('change'));
+    expect(input.value).toEqual('-12.41');
+  });
+});
+
+@Component({
+  // tslint:disable-next-line:component-selector
+  selector: 'test-cmp',
+  template: ``
+})
+class TestComponent {
+  constructor() {}
+}
+
+function createTestComponent(template: string): ComponentFixture<TestComponent> {
+  const component = TestBed.overrideComponent(TestComponent, { set: { template } }).createComponent(TestComponent);
+  component.detectChanges();
+  return component;
+}
+
+function getInput(fixture: ComponentFixture<TestComponent>, applyDirective: boolean): HTMLInputElement {
+  if (applyDirective) {
+    fixture = createTestComponent('<input id="test-input" dlNumericInput/>');
+  } else {
+    fixture = createTestComponent('<input id="test-input"/>');
+  }
+
+  const input: HTMLInputElement = (fixture.nativeElement as HTMLInputElement).querySelector('#test-input');
+  return input;
+}
+
+function dispatchInputEvent(
+  fixture: ComponentFixture<TestComponent>,
+  event: KeyboardEvent,
+  applyDirective: boolean
+): HTMLInputElement {
+  const input = getInput(fixture, applyDirective);
+  input.focus();
+  input.value = event.key;
+  input.dispatchEvent(event);
+  return input;
+}
