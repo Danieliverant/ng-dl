c59629ecc2363f0bd1b25bc82486f3aab2f82385
Daniel Liverant <daniel.liverant@justeattakeaway.com>
2021-02-22 18:18:27 +0200

    feat(drag-over): publish

--- projects/delayed-dragover/karma.conf.js
+++ projects/delayed-dragover/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../../coverage/numeric-input'),
+      dir: require('path').join(__dirname, '../../coverage/delayed-dragover'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
